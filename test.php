8bef1836992268584dc6717bfef004225aa7fadf
Biagem01 <43443161+Biagem01@users.noreply.github.com>
2025-06-09 09:35:06 +0200

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=localhost;port=8889;dbname=orizon", "root", "root");
-    echo "✅ Connessione riuscita!";
-} catch (PDOException $e) {
-    echo "❌ Errore di connessione: " . $e->getMessage();
-}
